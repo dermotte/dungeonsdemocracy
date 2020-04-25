6786243dc014b9e9f0b09e9979236db5bb2b3881
Mathias Lux <mathias@juggle.at>
2020-04-25 22:19:42 +0200

    added states

--- js/gamecontroller.js
+++ js/gamecontroller.js
@@ -2,3 +2,21 @@
 // <script type="text/javascript" src="js/gamecontroller.js"></script>
 // use
 // e.g. ...
+
+const session_states = {
+    lobby: "lobby",
+    writing: "writing",
+    voting: "voting",
+    highscore: "highscore"
+}
+
+var state = { // reflected in the session ...
+    game_state: session_states.lobby,
+    users: [{
+        is_writer: false,
+        name: "",
+        score: 0
+    }],
+    // readers: [], // all - writers
+    turn: 0
+}
