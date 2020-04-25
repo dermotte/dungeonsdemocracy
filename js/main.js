9f4b25b42bfe792a37f5dcc8a9a956d14ca4ff17
Mathias Lux <mathias@juggle.at>
2020-04-25 23:13:54 +0200

    added states

--- js/main.js
+++ js/main.js
@@ -72,8 +72,7 @@ function listenToMessages(sessionID) {
                 u = getUserData();
                 // if (change.doc.data().sessionID === u.sessionID) {
                 if (change.type === "added") {
-
-                    if(is_host([], getUserData().user)){
+                    if(is_host(db.collection("cities").doc(utils.getSessionID()).data().users, getUserData().user)){
 
                       // in gamecontroller.js
                       process_message(change.doc.data());
