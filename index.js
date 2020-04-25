05f26c3f59ad4981ad768af7d0a0a479d81d448b
Bernd Münzer <bernd@itec.aau.at>
2020-04-25 23:42:07 +0200

    login and random start sentences

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const fs = require('fs');
 
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -12,10 +13,8 @@ app.get('/', function(request, response){
     response.sendFile(__dirname+'/view/welcome.html');
 });
 
-// TODO login to obtain accessToken
-// TODO does this token stay constant or does it expire after some time??
+// default accessToken (can be changed by calling login)
 let accessToken = "0222bed0-8701-11ea-ad58-87aded6b30c5";
-let headers = {"headers": {"X-Access-Token" : accessToken}};    // authentication magic
 let sessionId;
 
 app.use(function(req, res, next) {
@@ -25,37 +24,59 @@ app.use(function(req, res, next) {
 });
 
 app.post('/login', function (req, res) {
-    res.send('TODO');
+    let credentials = req.body;
+
+    axios.post('https://api.aidungeon.io/users',
+        credentials
+    ).then((response) => {
+        if (response.data.accessToken) {
+            accessToken = response.data.accessToken;
+            console.log("new accessToken " + accessToken);
+            res.send(accessToken);
+        }
+    })
+    .catch((error) => {
+        console.error("login failed");
+        res.send("login failed");
+    });
 });
 
 app.post('/initStory', function (req, res) {
-
     try {
-        // TODO different choices (random? custom?)
+        let customPrompt = req.body.customPrompt;
+
+        // read random starting story from a file if nothing is passed
+        if (!customPrompt) {
+            customPrompt = getRandomStartSentence();
+        }
+
+        // let storyConfiguration = {
+        //     "storyMode": "fantasy",
+        //     "characterType": "knight",
+        //     "name": "Donald"
+        // }
+
         let storyConfiguration = {
-            "storyMode": "fantasy",
-            "characterType": "knight",
-            "name": "Donald"
+            "storyMode": "custom",
+            "customPrompt": customPrompt
         }
 
         // create a new session
         axios.post('https://api.aidungeon.io/sessions',
             storyConfiguration,
-            headers
-        )
-            .then((response) => {
-
+            getHeader()
+        ).then((response) => {
             sessionId = response.data.id;
-        console.log("new sessionId: " + sessionId);
+            console.log("new sessionId: " + sessionId);
 
-        let output = response.data.story[0].value;
-        console.log(output);
-        res.send(output);
-    })
-    .catch((error) => {
+            let output = response.data.story[0].value;
+            console.log(output);
+            res.send(output);
+        })
+        .catch((error) => {
             console.error(error)
-        res.send(error);
-    });
+            res.send(error);
+        });
     } catch (e) {
         res.send(e);
     }
@@ -65,7 +86,7 @@ app.post('/initStory', function (req, res) {
 app.post('/resumeRandomStory', function(req, res) {
 
     try {
-        axios.get('https://api.aidungeon.io/sessions', headers)
+        axios.get('https://api.aidungeon.io/sessions', getHeader())
             .then((response) => {
             // response.data is an array of all started stories of the current user, we only need the id
             let sessions = response.data;
@@ -76,12 +97,13 @@ app.post('/resumeRandomStory', function(req, res) {
         let sessionIds = response.data.map(s => s.id);
         let randomIdx = Math.floor(Math.random() * sessionIds.length);
         let randomId = sessionIds[randomIdx];
+
         sessionId = randomId;
         console.log("resuming session: " + randomId);
         res.send("resuming session: " + randomId);
     })
     .catch((error) => {
-            console.error(error)
+        console.error(error);
         res.send(error);
     });
 
@@ -104,15 +126,15 @@ app.post('/input', function (req, res) {
 
         axios.post('https://api.aidungeon.io/sessions/' + sessionId + '/inputs',
             data,
-            headers
+            getHeader()
         )
-            .then((response) => {
-            let output = response.data[response.data.length-1].value;   // get the most recent text
+        .then((response) => {
+        let output = response.data[response.data.length-1].value;   // get the most recent text
         console.log(output);
         res.send(output);
     })
     .catch((error) => {
-            console.error(error)
+        console.error(error)
         res.send(error);
     });
     } catch (e) {
@@ -122,5 +144,16 @@ app.post('/input', function (req, res) {
 });
 
 app.listen(PORT, function () {
-    console.log('ioDungeonProxy listening on port 3000!');
+    console.log('ioDungeonProxy listening on port ' + PORT);
 });
+
+function getHeader() {
+    return {"headers": {"X-Access-Token" : accessToken}};
+}
+
+function getRandomStartSentence() {
+    let sentences = fs.readFileSync('./startSentences.txt', 'utf8').split("\r\n").filter(s => s.length > 0);
+    let randomIdx = Math.floor(Math.random() * sentences.length);
+    let randomSentence = sentences[randomIdx];
+    return randomSentence;
+}
