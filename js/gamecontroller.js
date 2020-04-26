0b706e75b7f23b5f692ec9acf23e083fd388859c
Christof Karisch <ckarisch@outlook.com>
2020-04-26 14:41:54 +0200

    remove messages on win

--- js/gamecontroller.js
+++ js/gamecontroller.js
@@ -146,24 +146,12 @@ const process_message_update = (message) => {
       console.log("everybody voted");
 
       assingWinner();
+
+      state.messages = [];
       assignWriters();
 
       update_state(session_states.writing);
     }
-}
-
-function union_arrays (x, y) {
-  var obj = {};
-  for (var i = x.length-1; i >= 0; -- i)
-     obj[x[i]] = x[i];
-  for (var i = y.length-1; i >= 0; -- i)
-     obj[y[i]] = y[i];
-  var res = []
-  for (var k in obj) {
-    if (obj.hasOwnProperty(k))  // <-- optional
-      res.push(obj[k]);
-  }
-  return res;
 }
 
 function assingWinner() {
