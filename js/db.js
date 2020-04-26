2f7a2562e748f153bf2248354105952641ae0953
Christof Karisch <ckarisch@outlook.com>
2020-04-26 13:51:18 +0200

    hide all buttons

--- js/db.js
+++ js/db.js
@@ -45,7 +45,7 @@ async function voteFor(docID) {
     newVotes.push(u.user);
     var merger = data_cache[docID].ref.set({votes: newVotes}, {merge : true});
 
-    document.querySelector(".btn-like").style.display = "none";
+    document.querySelectorAll(".btn-like").style.display = "none";
 }
 
 async function addSession(sessionName) {
